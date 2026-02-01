0b8c983e5855d72dd7ba98bbf465ad68b9d27b80
aadeepaggarwal <aadeepaadeep@gmail.com>
2026-02-01 19:24:40 +0530

    Javascrpt

---
+++ 14_JavaScript/index.js
@@ -0,0 +1,19 @@
+// let a=10;
+var a=10;
+console.log(a);
+
+a="Hello"
+console.log(a);
+
+a=true;
+console.log(a);
+
+let arr=[1,2,3,4,5];
+console.log(arr[0])
+console.log(arr)
+
+let arr2=[1,2,3, "Hello", true, "Coding"]; // can have heterogenous arrays
+arr2[8]=20; // can have empty items
+
+console.log(arr2);
+
